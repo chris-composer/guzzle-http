cabfcd4275623a68a7a15cad349a9a797bee8406
crabapple <2276447099@qq.com>
2020-04-17 13:30:06 +0800

    init

---
+++ src/Exceptions/ComException.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace ChrisGuzzleHttp\Exceptions;
+
+use Symfony\Component\HttpKernel\Exception\HttpException;
+
+class ComException extends HttpException
+{
+    public $msg;
+    public $code;
+    public $response_code;
+
+    public function __construct($msg, $code, $response_code = 500)
+    {
+        parent::__construct($code);
+        $this->msg = $msg;
+        $this->code = $code;
+        $this->response_code = $response_code;
+    }
+}
---
+++ src/GuzzleHttpServer.php
@@ -0,0 +1,55 @@
+<?php
+
+
+namespace ChrisGuzzleHttp;
+
+
+use App\Exceptions\ComException;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
+
+class GuzzleHttpServer
+{
+    public static function request_http_get($uri, $option = [])
+    {
+        $client = new Client(['timeout' => 10.0]);
+
+        try {
+            $response = $client->request('get', $uri, $option);
+
+            return json_decode((string)$response->getBody(), true);
+        } catch (RequestException $e) {
+            throw new ComException($e->getMessage(), $e->getCode());
+        }
+    }
+
+    public static function request_http_post($uri, $params = [], $body_type = 'json')
+    {
+        $client = new Client(['timeout' => 10.0]);
+
+        try {
+            $post_data = [];
+
+            if ($params) {
+                if ($body_type === 'body') {
+                    $post_data = [
+                        'body' => json_encode($params, JSON_UNESCAPED_UNICODE)
+                    ];
+                }
+                elseif($body_type === 'json') {
+                    $post_data = [
+                        'json' => $params
+                    ];
+                }
+            }
+
+            $response = $client->request('post', $uri, $post_data);
+
+            $data = json_decode((string)$response->getBody(), true);
+
+            return $data;
+        } catch (RequestException $e) {
+            throw new ComException($e->getMessage(), $e->getCode());
+        }
+    }
+}
